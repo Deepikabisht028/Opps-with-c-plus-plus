832dff8e941637cf2d2b9bc64dea7d4c6a580c07
Deepikabisht028 <91360412+Deepikabisht028@users.noreply.github.com>
2022-01-05 00:17:35 +0530

    Add files via upload

---
+++ Practical15/15.2task3.cpp
@@ -0,0 +1,12 @@
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    fstream fout;  
+    fout.open ("file.txt",ios::app); 
+    if(fout.is_open())
+        fout<< "\n task3 to append a file\n"; 
+    fout.close(); 
+    return 0;
+}
