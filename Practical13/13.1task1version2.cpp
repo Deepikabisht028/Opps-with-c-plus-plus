94c9b2e0005206cc96fc48ecdbe61af99a69d4d5
Deepikabisht028 <91360412+Deepikabisht028@users.noreply.github.com>
2022-01-10 02:18:06 +0530

    Add files via upload

---
+++ Practical13/13.1task1version2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class base
+{    public:
+      int add(int a,int b)
+      {
+          return (a+b);
+      }
+      float add(float a,float b)
+      {
+          return (a+b);
+      }
+};
+class child:public base
+{    public:
+      using base::add;
+      int add(int a,int b)
+      {
+          return (a+b+1);
+      }
+};
+int main()
+{
+    child c;
+    cout<<"Addition of 2,3: "<<c.add(2,3)<<endl;
+    cout<<"Addition of 2.1,7.5: "<<c.add(2.1f,7.5f);
+}
+//child class can't see the add funtion of int and float return type of base class.
